5a3c9fe5f59a1c3befd8c5e43e8198df020b25e6
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-07-02 00:31:40 +0300

    lesson 34

---
+++ 34.Delete a Leaf Node in a Binary Search Tree.js
@@ -0,0 +1,31 @@
+var displayTree = tree => console.log(JSON.stringify(tree, null, 2));
+function Node(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+function BinarySearchTree() {
+    this.root = null;
+    // Only change code below this line
+    this.remove = function (value) {
+        if (!this.root) return null
+        let parent
+        let target = this.root
+        while (target && target.value !== value) {
+            parent = target
+            if (target.value > value) {
+                target = target.left
+            } else {
+                target = target.right
+            }
+        }
+        if (!target) return null
+        if (!parent) {
+            this.root = null
+        } else {
+            const direction = parent.left === target ? "left" : "right"
+            parent[direction] = null
+        }
+    }
+}
